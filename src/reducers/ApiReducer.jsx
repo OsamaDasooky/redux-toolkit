10076ffa2c7c624d252e5c794a11b68ed6d95e83
yousef <83317552+YousefK5@users.noreply.github.com>
2022-12-04 11:30:58 +0300

    add comment

--- src/reducers/ApiReducer.jsx
+++ src/reducers/ApiReducer.jsx
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+// state 
 const initialState = [];
 const ApiSlice = createSlice({
   name: "ApiMovies",
   initialState,
   reducers: {
+    // this method to save data in state 
     saveData: (state, action) => {
       state = state.push(action.payload);
     },
